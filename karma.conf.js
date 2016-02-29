dc128a2bcd30f84b49c24b49b5fe8a436c2719e2
crossjs <liwenfu@crossjs.com>
2016-02-29 20:56:48 +0800

    update scripts

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,9 @@
 'use strict';
 
+var argv = require('yargs').argv;
+
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -14,11 +13,12 @@ module.exports = function(config) {
     files: [
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
       './node_modules/sinon/pkg/sinon.js',
-      'tests/**/*.spec.js'
-    ],
-
-    // list of files to exclude
-    exclude: [
+      {
+        pattern: 'tests/**/*.spec.js',
+        watched: false,
+        served: true,
+        included: true
+      }
     ],
 
     // preprocess matching files before serving them to the browser
@@ -52,30 +52,13 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha', 'coverage'],
 
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
+    singleRun: !argv.watch,
 
     // optionally, configure the reporter
     coverageReporter: {
